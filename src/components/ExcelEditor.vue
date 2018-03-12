a50992a9dd2f62b675d1dd649dfd118ee1e76ea1
myliang <liangyuliang0335@126.com>
2018-03-12 20:49:52 +0800

    editor auto width

--- src/components/ExcelEditor.vue
+++ src/components/ExcelEditor.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="ve-edit" :style="{left: `${left - 1}px`, top: `${top - 1}px`}" v-if="visable">
     <textarea autofocus :value="value.text"
-      :style="[{width: `${width - 8}px`, height: `${height - 2}px`, 'line-height': `20px`}, cellStyle(value)]"
+      :style="[{'min-width': `${width}px`, width: `${width}px`, height: `${height}px`, 'line-height': `21px`}, cellStyle(value)]"
       @mousedown.stop="()=>{}"
-      @input="updateValue($event.target.value)"></textarea>
+      @input="updateValue($event.target.value)"
+      ref="input"></textarea>
+    <div style="visibility: hidden; position: fixed; top: 0; left: 0;" ref="iw">{{ value.text }}</div>
   </div>
 </template>
 <script>
@@ -20,14 +22,12 @@ export default {
       visable: true,
       top: offsetTop,
       left: offsetLeft,
-      width: offsetWidth,
-      height: offsetHeight
+      width: offsetWidth - 8,
+      height: offsetHeight - 2
     }
   },
-  watch: {
-    value (nval, oval) {
-      // console.log('nval:', nval)
-    }
+  mounted () {
+    this.reload()
   },
   methods: {
     reload () {
@@ -35,14 +35,29 @@ export default {
       const { offsetTop, offsetLeft, offsetWidth, offsetHeight } = this.target
       this.top = offsetTop
       this.left = offsetLeft
-      this.width = offsetWidth
-      this.height = offsetHeight
+      const clientWidth = document.documentElement.clientWidth
+      const maxWidth = clientWidth - this.left - 24
+      let ow = this.$refs.iw.offsetWidth + 12
+      // console.log(':::', ow, maxWidth, this.left, clientWidth)
+      if (ow > offsetWidth) {
+        if (ow > maxWidth) {
+          const h = (parseInt(ow / maxWidth) + 1) * 20
+          if (h > offsetHeight) {
+            this.$refs.input.style.height = `${h}px`
+          } else {
+            this.$refs.input.style.height = `${offsetHeight}px`
+          }
+          ow = maxWidth
+        }
+        this.$refs.input.style.width = `${ow}px`
+      }
     },
     updateValue (v) {
       this.changeHandler(v)
     },
     changeHandler (v) {
       this.value.text = v
+      this.reload()
     },
     cellStyle
   }
